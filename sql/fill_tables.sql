aa1338be9e5a0eb1443722105f79aa0c57132452
unknown <andreas.v.andra@gmail.com>
2021-10-08 14:09:59 +0300

    added coordinates to data generation

--- sql/fill_tables.sql
+++ sql/fill_tables.sql
@@ -1,109 +1,109 @@
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('lumo', 'redi tower 3', '{ "gmlid": "BID_3593141b-e49f-4aac-aa8b-ff663df696b5", "ratu": "68654" }');
+VALUES('Työpajankatu 17a', 'lumo one redi tower 3', '{ "gmlid": "BID_3593141b-e49f-4aac-aa8b-ff663df696b5", "ratu": "68654", "coordinates": [60.18765513370254, 24.978827621692176] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('majakka', 'redi tower 1', '{ "gmlid": "BID_4f77b0c3-812f-470b-b305-7c4807e2934f", "ratu": "68654" }');
+VALUES('Kalasatamankatu 9', 'majakka redi tower 1', '{ "gmlid": "BID_4f77b0c3-812f-470b-b305-7c4807e2934f", "ratu": "68654", "coordinates": [60.1874922366134, 24.97969281175711] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('loisto', 'redi tower 2', '{ "gmlid": "BID_8ca5150a-fd1c-4047-a038-4d39e8a81fe1", "ratu": "68654" }');
+VALUES('Englantilaisaukio 10', 'loisto redi tower 2', '{ "gmlid": "BID_8ca5150a-fd1c-4047-a038-4d39e8a81fe1", "ratu": "68654", "coordinates": [60.18725721680828, 24.98093599362924] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('1030141204', 'apartment building', '{ "gmlid": "BID_6f2f9ec4-bbdc-4da6-a41a-2c54ff7ee150", "ratu": "58302", "vtj-prt": "1030141204"  }');
+VALUES('Arielinkatu 2', 'HOAS apartment building', '{ "gmlid": "BID_6f2f9ec4-bbdc-4da6-a41a-2c54ff7ee150", "ratu": "58302", "vtj-prt": "1030141204", "coordinates": [60.18587427585493, 24.981382476847354] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('103023724X', 'apartment building', '{ "gmlid": "BID_0a3a6049-77d6-4c9e-8486-c3469fe49cc7", "ratu": "58145", "vtj-prt": "103023724X" }');
+VALUES('Capellan Puistotie 7', 'apartment building', '{ "gmlid": "BID_0a3a6049-77d6-4c9e-8486-c3469fe49cc7", "ratu": "58145", "vtj-prt": "103023724X", "coordinates": [60.185875314921006, 24.98301240227945] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('101902888X', 'apartment building', '{ "gmlid": "BID_d3ce9c37-e52e-44c4-a1fe-5b5e23e23ffb", "ratu": "60684", "vtj-prt": "101902888X" }');
+VALUES('Capellan Puistotie 13', 'Helsingin Harjus', '{ "gmlid": "BID_28a105c7-0695-49d3-ab90-c2ec7667fb03", "ratu": "65736", "vtj-prt": "103635712F", "coordinates": [60.187966171427234, 24.98216160460959] }');
 
 INSERT INTO featureofinterest(name, description, feature) 
-VALUES('60683', 'apartment building', '{ "gmlid": "BID_aaf93473-8f29-4ff0-8571-a40875316561", "ratu": "60683" }');
+VALUES('Capellanranta 3', 'apartment building', '{ "gmlid": "BID_1303d8da-6736-4749-9262-3ebd5acc9d4d", "ratu": "66279", "vtj-prt": "103655374R", "coordinates": [60.18853925084039, 24.98455853986285] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('61059', 'school', '{ "gmlid": "BID_13923f39-b605-4f81-9173-c14c21630685", "ratu": "61059" }');
+VALUES('Polariksenkatu 1', 'daycare', '{ "gmlid": "BID_13923f39-b605-4f81-9173-c14c21630685", "ratu": "61059", "coordinates": [60.18376024777178, 24.979743747461374] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('61059', 'daycare', '{ "gmlid": "BID_0f7ae73a-0cd8-4fb8-8f79-fb3caa6c79af", "ratu": "61059" }');
+VALUES('Polariksenkatu 1', 'school', '{ "gmlid": "BID_0f7ae73a-0cd8-4fb8-8f79-fb3caa6c79af", "ratu": "61059", "coordinates": [60.18424613685863, 24.979216087806908] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('103526938M', 'health center', '{ "gmlid": "BID_3224c543-6e8e-4e0d-a828-ca0742564036", "ratu": "63640", "vtj-prt": "103526938M" }');
+VALUES('Työpajankatu 14', 'health center', '{ "gmlid": "BID_3224c543-6e8e-4e0d-a828-ca0742564036", "ratu": "63640", "vtj-prt": "103526938M", "coordinates": [60.188104122016604, 24.980301004675688] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('59766', 'redi', '{ "gmlid": "BID_d6f03f71-d260-4525-9073-dc0cde28575e", "ratu": "59766" }');
+VALUES('Hermannin rantatie 5', 'redi', '{ "gmlid": "BID_d6f03f71-d260-4525-9073-dc0cde28575e", "ratu": "59766", "vtj-prt": "103201974X", "coordinates": [60.187447302577546, 24.97874927118575] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('n/a', 'office, under construction', '{ "gmlid": "BID_699d39fa-9ee2-446c-a019-4b1efb67b42a", "ratu": "n/a" }');
+VALUES('Viikinkaari 2', 'Ympäristötalo', '{ "gmlid": "BID_a03c680b-a156-473d-b210-ae261d808ebf", "ratu": "56181", "vtj-prt": "103126970E", "coordinates": [60.224864967383574, 25.0157006465795535] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('n/a', 'redi shopping center', '{ "gmlid": "BID_49a3253a-ffbe-49a7-a7bd-a6fdc0fd7f6e", "ratu": "n/a" }');
+VALUES('Viikinkaari 1', 'Biokeskus 3', '{ "gmlid": "BID_0ba21978-3711-4e8e-9aa9-28a1101e2ebf", "ratu": "45753", "vtj-prt": "1031270202", "coordinates": [60.22550452681764, 25.01723687974716] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('n/a', 'apartment building', '{ "gmlid": "BID_6e5c165d-9eb9-4b3d-9a4b-76958e13b4df", "ratu": "n/a" }');
+VALUES('Pasteurinkatu 1', 'Hoas apartment building', '{ "gmlid": "BID_c4d319db-2f94-44f6-a0e0-966053a23e4b", "ratu": "51824", "vtj-prt": "103126974K", "coordinates": [60.224692886152766, 25.01495894723165] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('1031518888', 'apartment building', '{ "gmlid": "BID_d1ebc713-dbbb-4e42-95d4-4b00e70dc67f", "ratu": "57729" }');
+VALUES('Pasteurinkatu 1', 'Hoas apartment building', '{ "gmlid": "BID_48605c90-e5e9-46a7-9255-9884865508d3", "ratu": "51823", "vtj-prt": "103126973J", "coordinates": [60.22459644697701, 25.014326770514675] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('1031518877', 'apartment building', '{ "gmlid": "BID_45df470a-9e9f-4bb4-82b0-d7b2874f97a6", "ratu": "57728", "vtj-prt": "103526938M" }');
+VALUES('Pasteurinkatu 1', 'Hoas apartment building', '{ "gmlid": "BID_c4d319db-2f94-44f6-a0e0-966053a23e4b", "ratu": "51822", "vtj-prt": "103126972H", "coordinates": [60.22458285180498, 25.013623619257693] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('n/a', 'apartment building', '{ "gmlid": "BID_f76c9ab3-beb9-4be5-894b-0099d8b4303c", "ratu": "n/a" }');
+VALUES('Pasteurinkatu 1', 'Hoas apartment building', '{ "gmlid": "BID_c4d319db-2f94-44f6-a0e0-966053a23e4b", "ratu": "51821", "vtj-prt": "103126971F", "coordinates": [60.22464960839552, 25.01300654496274] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('n/a', 'apartment building', '{ "gmlid": "BID_0b3e6442-6beb-4d51-9da5-2f92c5264482", "ratu": "n/a" }');
+VALUES('Arcturuksenkatu 6', 'Hoas apartment building', '{ "gmlid": "BID_059d0a21-a2a7-4aa5-b427-2975517f09ba", "ratu": "62413", "vtj-prt": "102821537S", "coordinates": [60.18350545990462, 24.980731448696897] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('n/a', 'apartment building', '{ "gmlid": "BID_e89fde40-8ae2-4f20-a409-62b129b13f72", "ratu": "n/a" }');
+VALUES('Koetilantie 1', 'Gardenia', '{ "gmlid": "BID_44e21d7d-3d6a-40f4-9b1d-0449f727684e", "ratu": "43970", "vtj-prt": "103126975L", "coordinates": [60.22398093600276, 25.015015309527623] }');
 
 /* Features with correct ratu/vtj-prt! */
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('101589033K', 'apartment building', '{ "gmlid": "BID_a12c5958-35a0-482d-a6d4-452b2cfb0d77", "ratu": "57968", "vtj-prt": "101589033K" }');
+VALUES('Arielinkatu 10', 'apartment building', '{ "gmlid": "BID_a12c5958-35a0-482d-a6d4-452b2cfb0d77", "ratu": "57968", "vtj-prt": "101589033K", "coordinates": [60.184810588271375, 24.979200104000128] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('103151890A', 'apartment building', '{ "gmlid": "BID_fa9d1697-4297-4949-9a81-e4eed574e177", "ratu": "57818", "vtj-prt": "103151890A" }');
+VALUES('Junonkatu 3', 'apartment building', '{ "gmlid": "BID_fa9d1697-4297-4949-9a81-e4eed574e177", "ratu": "57818", "vtj-prt": "103151890A", "coordinates": [60.184746802489265, 24.980044779803592] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('101773340Y', 'apartment building', '{ "gmlid": "BID_ef92ed39-1569-4773-834a-f916dd4dfa6a", "ratu": "61452", "vtj-prt": "101773340Y" }');
+VALUES('Arielinkatu 16', 'apartment building', '{ "gmlid": "BID_ef92ed39-1569-4773-834a-f916dd4dfa6a", "ratu": "61452", "vtj-prt": "101773340Y", "coordinates": [60.18354778512082, 24.977489438734207] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('1032851589', 'apartment building', '{ "gmlid": "BID_87552cc2-eb82-4e36-aa52-9e478985d931", "ratu": "62547", "vtj-prt": "1032851589" }');
+VALUES('Arcturuksenkatu 5', 'apartment building', '{ "gmlid": "BID_87552cc2-eb82-4e36-aa52-9e478985d931", "ratu": "62547", "vtj-prt": "1032851589", "coordinates": [60.18349418645755, 24.978718756465245] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('1017733421', 'apartment building', '{ "gmlid": "BID_1414245b-1796-4bac-a3af-a4bc9046773e", "ratu": "61454", "vtj-prt": "1017733421" }');
+VALUES('Parrulaituri 10', 'apartment building', '{ "gmlid": "BID_1414245b-1796-4bac-a3af-a4bc9046773e", "ratu": "61454", "vtj-prt": "1017733421", "coordinates": [60.18316236529791, 24.97856186060175] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('1017733410', 'apartment building', '{ "gmlid": "BID_0f7bfad2-a770-470d-96ae-4a965f68429d", "ratu": "61453", "vtj-prt": "1017733410" }');
+VALUES('Junonkatu 9', 'apartment building', '{ "gmlid": "BID_0f7bfad2-a770-470d-96ae-4a965f68429d", "ratu": "61453", "vtj-prt": "1017733410", "coordinates": [60.183375887636146, 24.979612028922027] }');
 
 /*2nd row new */
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('103305566L', 'apartment building', '{ "gmlid": "BID_5908c7b2-2810-4659-a241-cd31fddaf19b", "ratu": "58676", "vtj-prt": "103305566L" }');
+VALUES('Junonkatu 2', 'apartment building', '{ "gmlid": "BID_5908c7b2-2810-4659-a241-cd31fddaf19b", "ratu": "58676", "vtj-prt": "103305566L", "coordinates": [60.185118120759036, 24.9812099886035] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('103305567M', 'apartment building', '{ "gmlid": "BID_84c0879b-667d-4052-b22c-870ed33a4005", "ratu": "58892", "vtj-prt": "103305567M" }');
+VALUES('Capellan Puistotie 8', 'apartment building', '{ "gmlid": "BID_84c0879b-667d-4052-b22c-870ed33a4005", "ratu": "58892", "vtj-prt": "103305567M", "coordinates": [60.184624240154385, 24.982310077421175] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('103423483C', 'apartment building', '{ "gmlid": "BID_95c19986-26fe-47ea-8181-469586a18581", "ratu": "59086", "vtj-prt": "103423483C" }');
+VALUES('Capellan Puistotie 10', 'apartment building', '{ "gmlid": "BID_95c19986-26fe-47ea-8181-469586a18581", "ratu": "59086", "vtj-prt": "103423483C", "coordinates": [60.18402344568273, 24.98206035135864 }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('103423483C', 'apartment building', '{ "gmlid": "BID_95c19986-26fe-47ea-8181-469586a18581", "ratu": "59086", "vtj-prt": "103423483C" }');
+VALUES('Polariksenkatu 9', 'apartment building', '{ "gmlid": "BID_63ed77d0-d840-4d64-8de4-9df4b004416e", "ratu": "66213", "vtj-prt": "1036517561", "coordinates": [60.18377792056886, 24.981125069931377] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('103423482B', 'apartment building', '{ "gmlid": "BID_fcdec70c-ff33-4849-b60e-acd58112f7e4", "ratu": "59082", "vtj-prt": "103423482B" }');
+VALUES('Junonkatu 4', 'apartment building', '{ "gmlid": "BID_fcdec70c-ff33-4849-b60e-acd58112f7e4", "ratu": "59082", "vtj-prt": "103423482B", "coordinates": [60.184413737653756, 24.980781590226076] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('102274850P', 'apartment building', '{ "gmlid": "BID_d766a7f7-1f7e-420b-82ff-f50acb2f800b", "ratu": "59083", "vtj-prt": "102274850P" }');
+VALUES('Antareksenkatu 9', 'apartment building', '{ "gmlid": "BID_d766a7f7-1f7e-420b-82ff-f50acb2f800b", "ratu": "59083", "vtj-prt": "102274850P", "coordinates": [60.18435439860013, 24.981580790524067] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('102274851R', 'apartment building', '{ "gmlid": "BID_707cb3d8-9ae3-4d3f-ba03-4884735b9fc8", "ratu": "59084", "vtj-prt": "102274851R" }');
+VALUES('Polariksenkatu 8', 'apartment building', '{ "gmlid": "BID_707cb3d8-9ae3-4d3f-ba03-4884735b9fc8", "ratu": "59084", "vtj-prt": "102274851R", "coordinates": [60.18398555846456, 24.981283256391283] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('103509649X', 'apartment building', '{ "gmlid": "BID_44a7f916-3b44-434b-8676-50058ffc8329", "ratu": "63304", "vtj-prt": "103509649X" }');
+VALUES('Capellan Puistotie 1/Leonkatu 21', 'apartment building', '{ "gmlid": "BID_44a7f916-3b44-434b-8676-50058ffc8329", "ratu": "63304", "vtj-prt": "103509649X", "coordinates": [60.18460291343378, 24.98301929685274] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('103529164E', 'apartment building', '{ "gmlid": "BID_c6c36d17-6fc8-4d7b-a0c9-182adc969178", "ratu": "63915", "vtj-prt": "103529164E" }');
+VALUES('Sörnäistenlaituri 5', 'apartment building', '{ "gmlid": "BID_c6c36d17-6fc8-4d7b-a0c9-182adc969178", "ratu": "63915", "vtj-prt": "103529164E", "coordinates": [60.18422781039279, 24.98363707080518] }');
 
 INSERT INTO featureofinterest (name, description, feature) 
-VALUES('103557252H', 'apartment building', '{ "gmlid": "BID_04b55dd6-c136-49a6-b142-723c0eb5ee89", "ratu": "64497", "vtj-prt": "103557252H" }');
+VALUES('Capellanaukio 1', 'apartment building', '{ "gmlid": "BID_04b55dd6-c136-49a6-b142-723c0eb5ee89", "ratu": "64497", "vtj-prt": "103557252H", "coordinates": [60.184123347693394, 24.98310091824768] }');
 
 INSERT INTO datastream(unitofmeasurement)
 SELECT 'http://finto.fi/ucum/en/page/r418'
